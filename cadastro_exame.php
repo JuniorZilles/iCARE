e13d3e168f12c1a3b40f6defdaec293378feb73d
JuniorZilles <junior_zilles@hotmail.com>
2020-07-12 10:11:39 -0300

    bugfix quando ia pra tela não tinha como voltar

--- cadastro_exame.php
+++ cadastro_exame.php
@@ -40,14 +40,14 @@ if (!isset($_SESSION['user'])) {
 <body>
     <div class="container">
         <nav class="navbar navbar-dark bg-dark">
-            <a class="navbar-brand" href="<?php echo $_SERVER["PHP_SELF"]; ?>">iCARE</a>
+            <a class="navbar-brand" href="home.php">iCARE</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo $_SERVER["PHP_SELF"]; ?>">Home</a>
+                        <a class="nav-link" href="home.php">Home</a>
                     </li>
                     <?php
                     if ($_SESSION['tipo'] == 'admin') {
