4963d3a3bd0e20ca1f142e5c1020e45f68f12c87
JuniorZilles <junior_zilles@hotmail.com>
2020-07-25 16:45:39 -0300

    AJUSTES

--- estatisticas.php
+++ estatisticas.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 //mostrar por usuario os gráficos e a quantidade de consultas
-
-require_once '_utilities.php';
+//Número de Consultas Mensais por Médico (admin e medico)
+//Número de Exames Mensais por Laboratório (laboratorio e admin)
+//Número de Exames Mensais por Paciente (laboratorio e admin)
+//Número de Consultas Mensais por Paciente (admin e medico)
 require_once '_menu.php';
 
 if (!isset($_SESSION['user'])) {
@@ -17,15 +19,13 @@ if (!isset($_SESSION['user'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap -->
-    <!-- CSS only -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
-    <!-- JS, Popper.js, and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    <script src=""></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="estatisticas.js"></script>
     <?php
     if (isset($_SESSION['erro'])) {
         echo $_SESSION['erro'];
@@ -49,8 +49,6 @@ if (!isset($_SESSION['user'])) {
                     <?php
                     if ($_SESSION['tipo'] == 'admin') {
                         echo makemenuadmin();
-                    } else if ($_SESSION['tipo'] == 'paciente') {
-                        echo makemenupaciente();
                     } else if ($_SESSION['tipo'] == 'laboratorio') {
                         echo makemenulaboratorio();
                     } else if ($_SESSION['tipo'] == 'medico') {
@@ -65,7 +63,15 @@ if (!isset($_SESSION['user'])) {
         </nav>
     </div>
     <br>
-
+    <!-- https://www.chartjs.org/samples/latest/ -->
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center">Visualização de dados</h5>
+                <canvas id="myChart"></canvas>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
