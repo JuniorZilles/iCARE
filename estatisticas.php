29ff15283d7faa220edb629badcec2409fe043f2
JuniorZilles <junior_zilles@hotmail.com>
2020-08-17 20:14:17 -0300

    backend funcionando do estatisticas

--- estatisticas.php
+++ estatisticas.php
@@ -13,6 +13,8 @@ if (!isset($_SESSION['user'])) {
     $_SESSION['erro'] = maketoast('Usuário não logado', 'Necessário realizar login para utilizar os recursos!');
     header("Location: index.php");
 }
+
+$object = get_data($_SESSION['user'], $_SESSION['tipo']);
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +30,6 @@ if (!isset($_SESSION['user'])) {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> -->
     <script src="estatisticas.js"></script>
     <?php
     if (isset($_SESSION['erro'])) {
@@ -69,7 +70,6 @@ if (!isset($_SESSION['user'])) {
         </nav>
     </div>
     <br>
-    <!-- https://www.chartjs.org/samples/latest/ -->
     <div class="container">
         <div class="card">
             <div class="card-body">
@@ -94,7 +94,7 @@ if (!isset($_SESSION['user'])) {
                     </thead>
                     <tbody>
                         <?php
-                        $object = get_data($_SESSION['user'], $_SESSION['tipo']);
+                        
 
                         //     for ($i = 0; $i < count($_users); $i++) {
                         //         $tipo = '';
