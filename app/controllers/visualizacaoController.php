186ca31da4e9c2851249e68081c4ea23f22c9a48
Junior Zilles <junior_zilles@hotmail.com>
2021-02-03 18:46:40 -0300

    fix: include exception

--- app/controllers/visualizacaoController.php
+++ app/controllers/visualizacaoController.php
@@ -5,16 +5,21 @@ class VisualizacaoController extends Controller
 {
     public function index()
     {
-        if (!isset($_SESSION['user'])) {
-            $_SESSION['erro'] = makeerrortoast('Usuário não logado');
-            header('Location: ../index.php?pag=Login');
-        } elseif ($_SESSION['tipo'] != 'admin') {
-            $_SESSION['erro'] = makeerrortoast('Usuário não permitido');
+        try {
+            if (!isset($_SESSION['user'])) {
+                $_SESSION['erro'] = makeerrortoast('Usuário não logado');
+                header('Location: ../index.php?pag=Login');
+            } elseif ($_SESSION['tipo'] != 'admin') {
+                $_SESSION['erro'] = makeerrortoast('Usuário não permitido');
+                header('Location: ../index.php?pag=Home');
+            } else {
+                $model = new VisualizacaoModel();
+                $dado = $model->getPessoas();
+                $this->carregarTemplate('visualizacao', array($dado));
+            }
+        } catch (Exception $e) {
+            $_SESSION['erro'] = makeerrortoast($e->getMessage());
             header('Location: ../index.php?pag=Home');
-        } else {
-            $model = new VisualizacaoModel();
-            $dado = $model->getPessoas();
-            $this->carregarTemplate('visualizacao', array($dado));
         }
     }
 }
